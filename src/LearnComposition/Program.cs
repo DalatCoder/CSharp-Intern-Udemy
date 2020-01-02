a05adeb3f13e3c4d06216f133eda472924b85955
DalatCoder <hieuntctk42@gmail.com>
2020-01-02 14:48:26 +0700

    Composition

--- src/LearnComposition/Program.cs
+++ src/LearnComposition/Program.cs
@@ -2,11 +2,53 @@
 
 namespace LearnComposition
 {
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class DbMigrator
+    {
+        private readonly Logger logger;
+
+        public DbMigrator(Logger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Migrate()
+        {
+            logger.Log("We are migrating ...");
+        }
+    }
+
+    public class Installer
+    {
+        private readonly Logger logger;
+
+        public Installer(Logger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Install()
+        {
+            logger.Log("We are installing the application.");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dbMigrator = new DbMigrator(new Logger());
+            var installer = new Installer(new Logger());
+
+            dbMigrator.Migrate();
+            installer.Install();
         }
     }
 }
