2ba5d35c301652b229063c655727591eca9c8fe1
DalatCoder <hieuntctk42@gmail.com>
2020-01-02 18:05:39 +0700

    Init state

---
+++ src/MethodOverriding/Canvas.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace MethodOverriding
+{
+    public class Canvas
+    {
+        public void DrawShape(List<Shape> shapes)
+        {
+            foreach (var shape in shapes)
+            {
+                switch (shape.Type)
+                {
+                    case ShapeType.Circle:
+                        Console.WriteLine("Draw a circle.");
+                        break;
+                    case ShapeType.Rectangle:
+                        Console.WriteLine("Draw a rectangle.");
+                        break;
+                    case ShapeType.Triangle:
+                        Console.WriteLine("Draw a triangle.");
+                        break;
+                }
+            }
+        }
+    }
+}
