f8d0950845b60b24c3361a11c74e49399fdfd568
DalatCoder <hieuntctk42@gmail.com>
2020-01-02 14:28:00 +0700

    Initial Commit

---
+++ src/LearnInheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
