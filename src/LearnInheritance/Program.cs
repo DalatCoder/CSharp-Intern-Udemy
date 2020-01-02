5274f39c3d2d6667eb299ec94ef1fb0106f24d8f
DalatCoder <hieuntctk42@gmail.com>
2020-01-02 14:37:23 +0700

    Inheritance

--- src/LearnInheritance/Program.cs
+++ src/LearnInheritance/Program.cs
@@ -2,11 +2,47 @@
 
 namespace LearnInheritance
 {
-    class Program
+    public class PresentationObject
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Object copied to clipboard!");
+        }
+
+        public void Duplicate()
+        {
+            Console.WriteLine("Object was duplicated.");
+        }
+    }
+
+    public class Text : PresentationObject
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+        public void AddHyperlink(string url)
+        {
+            Console.WriteLine($"We added a link to {url}");
+        }
+    }
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var text = new Text()
+            {
+                Width = 100,
+                Height = 50,
+                FontName = "Arial",
+                FontSize = 11
+            };
+
+            text.Copy();
+            text.Duplicate();
         }
     }
 }
