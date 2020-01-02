6f0de9b2904c60730237d9c1339b28e4178877ca
DalatCoder <hieuntctk42@gmail.com>
2020-01-02 18:03:45 +0700

    Init state

--- src/MethodOverriding/Program.cs
+++ src/MethodOverriding/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MethodOverriding
 {
+    public class Position
+    {
+
+    }
+
+    public enum ShapeType
+    {
+        Circle,
+        Rectangle,
+        Triangle
+    }
+
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Position Position { get; set; }
+        public ShapeType Type { get; set; }
+    }
+
+    public class Canvas
+    {
+        public void DrawShape(List<Shape> shapes)
+        {
+            foreach (var shape in shapes)
+            {
+                switch (shape.Type)
+                {
+                    case ShapeType.Circle:
+                        Console.WriteLine("Draw a circle.");
+                        break;
+                    case ShapeType.Rectangle:
+                        Console.WriteLine("Draw a rectangle.");
+                        break;
+                    case ShapeType.Triangle:
+                        Console.WriteLine("Draw a triangle.");
+                        break;
+                }
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
