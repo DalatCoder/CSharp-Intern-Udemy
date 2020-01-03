7534a79e46da754f3f5303f90923b7b7a0320e2f
DalatCoder <hieuntctk42@gmail.com>
2020-01-03 10:31:11 +0700

    Interface

--- src/LearnPolymorphism/Program.cs
+++ src/LearnPolymorphism/Program.cs
@@ -1,12 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LearnPolymorphism
 {
+    public interface INotificationChannel
+    {
+        void Send(Message message);
+    }
+
+    public class Message
+    {
+    }
+
+    public class MailNotificationChannel : INotificationChannel
+    {
+        public void Send(Message message)
+        {
+            Console.WriteLine("Sending mail...");
+        }
+    }
+
+    public class SmsNotificationChannel : INotificationChannel
+    {
+        public void Send(Message message)
+        {
+            Console.WriteLine("Sending SMS...");
+        }
+    }
+
+    public class MailService
+    {
+        public void Send(Mail mail)
+        {
+            Console.WriteLine("Sending email... ");
+        }
+    }
+
+    public class Mail
+    {
+
+    }
+
+    public class Video
+    {
+
+    }
+
+    public class VideoEncoder
+    {
+        private readonly IList<INotificationChannel> _notificationChannels;
+        public VideoEncoder()
+        {
+            _notificationChannels = new List<INotificationChannel>();
+        }
+
+        public void Encode(Video video)
+        {
+            // Video encoding logic ...
+            foreach (var channel in _notificationChannels)
+            {
+                channel.Send(new Message());
+            }
+        }
+
+        public void RegisterNotificationChannel(INotificationChannel channel)
+        {
+            _notificationChannels.Add(channel);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var encoder = new VideoEncoder();
+            encoder.RegisterNotificationChannel(new MailNotificationChannel());
+            encoder.RegisterNotificationChannel(new SmsNotificationChannel());
+            encoder.Encode(new Video());
         }
     }
 }
