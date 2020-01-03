31117c1cb7461dc2c7e9410e4bb0dcb09f3166fa
DalatCoder <hieuntctk42@gmail.com>
2020-01-03 09:38:11 +0700

    Init Commit

---
+++ src/LearnPolymorphism/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnPolymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
