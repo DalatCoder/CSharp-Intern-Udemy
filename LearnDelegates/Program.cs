651c4e6ba72f1f2612a7f09fb2ec06b3fa00bc5a
DalatCoder <hieuntctk42@gmail.com>
2020-01-03 15:34:34 +0700

    Init state

--- LearnDelegates/Program.cs
+++ LearnDelegates/Program.cs
@@ -2,11 +2,59 @@
 
 namespace LearnDelegates
 {
+    public class Photo
+    {
+        public static Photo Load(string path)
+        {
+            return new Photo();
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saving photo...");
+        }
+
+    }
+
+    public class PhotoProcessor
+    {
+        public void Process(string path)
+        {
+            var photo = Photo.Load(path);
+
+            var filter = new PhotoFilters();
+            filter.ApplyBrightness(photo);
+            filter.ApplyContrast(photo);
+            filter.Resize(photo);
+
+            photo.Save();
+        }
+    }
+
+    internal class PhotoFilters
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Apply brightness");
+        }
+
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Apply contrast");
+        }
+
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Resize");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var processor = new PhotoProcessor();
+            processor.Process("");
         }
     }
 }
